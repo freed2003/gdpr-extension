dc8e1a825921fd55926c73e97f0033d3dcb620b5
freed2003 <gary.song2003@gmail.com>
2023-07-13 18:51:20 -0700

    more setup

---
+++ backend/api.py
@@ -0,0 +1,10 @@
+import selenium
+
+
+#return a boolean on if it passes and probably other data i haven't decided on
+def wpcheck(url: str) -> dict:
+    pass
+
+
+def policycheck(url: str) -> dict:
+    pass
--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
+
+
+@app.route("/api/v1/", methods=['GET'])
+def check():
+    res = {}
+    args = request.args
+    url = args.get("url")
+    wpcheck = wpcheck(url)
+    policycheck = policycheck(url)
